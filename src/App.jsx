e326ec866a2d32e1d744066c870300e505e5a5d0
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-09-11 18:12:18 +0530

    add side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./components/shared/Nav";
+import SideBar from "./components/shared/SideBar";
 
 function App() {
   return (
     <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <Nav />
+      <SideBar />
     </div>
   );
 }
